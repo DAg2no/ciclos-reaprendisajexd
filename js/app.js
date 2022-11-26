47c9c837816aec7ca87986a66b0b9ef420364f9a
DAg2no <dualwinder777.2.0@gmail.com>
2022-11-25 22:56:49 -0300

    hola

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
-function funcionexperimental() {
-    console.log(['pablo', 'juan', 'diego', 'pedro'].length);
-    while(funcionexperimental){
-        case "pablo"
-            
-    }
+let box = document.getElementById('box');
+    box.innerText = ('');
+
+console.log(box)
+
+const Names = ['thomas', 'mario', 'juan', 'pablo']
+if (Names[0] == box) {
+    console.log('correct')
+} else {
+    console.log('incorrect')
 }
